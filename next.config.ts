3dad6f42761c6d5ce6c13b9e2b61126616b28d15
barath <skbarath424@gmail.com>
2026-05-24 21:38:48 +0530

    1

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,6 @@ const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   basePath: basePath,
-  assetPrefix: basePath ? `${basePath}/` : undefined,
   images: {
     unoptimized: true,
     remotePatterns: [
